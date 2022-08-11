2ebc526defc5dea152fa5a0350264d41ebec17dd
Ivan <soldren2@gmail.com>
2022-08-11 19:27:17 +0300

    objektiv 15

--- Program.cs
+++ Program.cs
@@ -1,14 +1,134 @@
-﻿// Задача 8
+﻿// Задача 15
 
-// К сожалению с данной задачей  воникли наибольшие трудности,  так как я не смог  подставить туда  сразу 
-// второй комплект  чисел
+using static System.Console;
+Clear();
+WriteLine("Введите  день недели");
+int number=int.Parse(ReadLine());
+string a="Понедельник";
 
-for (int x = 1; x < 5; x++)
+string b="Вторник";
+string c="Среда";
+string d="Четверг";
+string e="Пятница";
+string f="Суббота";
+string g="Воскресенье";
+int numberB=6;
+int numberA=7;
 
- if (x % 2 == 0)
-  Console.WriteLine("вывод x " + x);
+int max1=1;
+
+if(number==max1)
+{
+  
+    WriteLine(a);
+    
+    if(max1<numberA && max1<numberB )
+    {
+        WriteLine("Это не выходной");
+    }
+    
+
+    
+} 
 
 
 
+
+int max2=2;
+
+
+if(number==max2)
+{
+
+    WriteLine(b);
+
+
+
+    if(max2<numberA && max2<numberB )
+    {
+        WriteLine("это не выходной");
+    }
+    
+} 
+
+
+
+int max3=3;
+if(number==max3)
+{
+    WriteLine(c);
+
+
+    if(max3<numberA && max3<numberB )
+    {
+        WriteLine("это не выходной");
+    }
+}
+int max4=4;
+if(number==max4)
+{
+    WriteLine(d);
+    
+if(max4<numberA && max4<numberB )
+    {
+        WriteLine("это не выходной");
+    }
+
+}
+
+int max5=5;
+if(number==max5)
+{
+    WriteLine(e);
+
+    if(max5<numberA && max5<numberB )
+    {
+        WriteLine("это не выходной");
+    }
+
+
+
+}
+
+int max6=6;
+if(number==max6)
+{
+    WriteLine(f);
+
+    if(max6<numberA && max6<numberB )
+    {
+        WriteLine("это не выходной");
+    }
+    else
+    {
+        WriteLine("это выходной");
+
+    }
+    
+
+    
+}
+
+int max7=7;
+if(number==max7)
+{
+    WriteLine(g);
+
+    if(max7<numberA && max7<numberB )
+    {
+        WriteLine("это не выходной");
+    }
+    else
+    {
+        WriteLine("это выходной");
+
+    }
+
+
+
+    
+    
+    
+} 
 
 
