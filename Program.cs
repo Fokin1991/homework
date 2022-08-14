abdfac2687503bfe1fd25bf1f84c8d568bd6ab41
Ivan <soldren2@gmail.com>
2022-08-14 18:54:39 +0300

    add

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
-﻿// Зaдача 10
+﻿// Зaдача 13
 
 using static System.Console;
 Clear();
-WriteLine("Введите трехзначное число");
+WriteLine("Введите  число");
 int numberA=Convert.ToInt32((Console.ReadLine()));
 
 string numberB=Convert.ToString(numberA);
-WriteLine(numberB[1]); 
+
+if(numberB.Length>2)
+{
+    WriteLine(numberB[2]);
+}
+
+else
+{
+    WriteLine("третьей цифры нет");
+}
+
+    
+    
