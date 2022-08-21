a4d9ee40efc09a4a37548f6c0631fb4fbf4a4d17
Ivan <soldren2@gmail.com>
2022-08-21 15:14:52 +0300

    objektiv 25

--- Program.cs
+++ Program.cs
@@ -1,19 +1,31 @@
 ﻿using static System.Console;
 
 Clear();
-WriteLine("введите  два числа");
 
+WriteLine("Введите  два число");
 int NumberA=Convert.ToInt32(ReadLine());
+
+WriteLine("Введите степень числа");
+
 int NumberB=Convert.ToInt32(ReadLine());
 
+int NumberC=exponentiate(NumberA,NumberB);
+WriteLine($"Натуральное степень  числа {NumberA} это {NumberC}");
 
-int exponentiate(int NumberA , int NumberB)
+
+
+
+
+
+int exponentiate(int NumberA, int NumberB)
 
 {
-    int result=0;
-for (int i = 0;  NumberA<NumberB; i++)
+    int result=1;
+for (int i = 0;  i<NumberB; i++)
 {
+    result*=NumberA;
     
+    double B=Math.Pow(NumberA,NumberB);
 }
     return result;
 }
