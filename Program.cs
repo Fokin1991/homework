f8831935c561b33e57e794557b051e293159c887
Ivan <soldren2@gmail.com>
2022-08-23 16:40:50 +0300

    add

--- Program.cs
+++ Program.cs
@@ -1,39 +1,23 @@
-﻿//Задача 27
+﻿//Задача 34
 
-//Не уверен, что я до конца  справился с  поставленной  задачей, но  вот  такие мысли у меня родились 
+
 
 using static System.Console;
+
 Clear();
-WriteLine("Введите  число");
-int NumberA=Convert.ToInt32(ReadLine());
-int NumberC=GetSum(NumberA);
-WriteLine(NumberC);
+
+int[] array=new int[3];
 
 
 
-
-
-
-
-
-int GetSum(int NumberA)
+int Getarray(int array)
 {
-    int result=0;
-    int index=0;
-    string B=Convert.ToString(NumberA);
+    int count=0;
     
-
-   
-while (index<B.Length)
-{
-    result+=NumberA%10;
-    NumberA/=10;
-    index++;
 }
 
 
-    return result;
-}
+
 
 
 
