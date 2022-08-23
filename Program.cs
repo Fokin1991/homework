9027caeec857c64959c935b910d3bcd67706b112
Ivan <soldren2@gmail.com>
2022-08-23 20:23:26 +0300

    objektiv-34

--- Program.cs
+++ Program.cs
@@ -8,23 +8,55 @@ Clear();
 
 int[] array=new int[3];
 
-
-
-int Getarray(int array)
-{
-    int count=0;
-    
-}
+FillArray(array);
+PrintArray(array);
+int NumberA=DigitCount(array);
 
 
 
-
-
-
+ void FillArray(int[] array)
+ {
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=new Random().Next(100,1000);
+    }
+ }
+  void PrintArray(int[] array)
+  {
+    for (int i = 0; i < array.Length; i++)
+    {
+       
+    }
+  }
 
  
 
 
+
+
+
+int  DigitCount(int [] array)
+{
+    int result=0;
+      Write("[");
+    for (int i = 0; i < array.Length; i++)
+   
+    {
+        if(array[i]%2==0)
+        result++;
+        
+        Write($" {array[i]}");
+       
+    } Write ("] ");
+    
+     
+
+
+
+    return result;
+
+}
+Write ($"{NumberA}-Это количество  четных  чисел  в массиве");
 
 
 
