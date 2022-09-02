9d584808be98a1e15d06be99708736a2c74df6d0
Ivan <soldren2@gmail.com>
2022-09-02 09:31:03 +0300

    add

--- Program.cs
+++ Program.cs
@@ -10,15 +10,39 @@ using static System.Console;
 using System.Linq;
 
 Clear();
-WriteLine("Введите число b1");
-double b1=Convert.ToInt32(ReadLine());
-WriteLine("Введите число к1");
-double k1=Convert.ToInt32(ReadLine());
-WriteLine("Введите число b2");
-double b2=Convert.ToInt32(ReadLine());
-WriteLine("Введите число k2"); 
-double k2=Convert.ToInt32(ReadLine());
-double x=(b2-b1)/(k1-k2);
-double y=k1*x+b1;
+WriteLine("Введите размер масива");
+int m=Convert.ToInt32(ReadLine());
+int n=Convert.ToInt32(ReadLine());
+WriteLine();
+double[,] array=new double[m,n];
+GetMatrixArray(array);
+PrintMatrixArray(array);
 
-WriteLine($"Координаты точек пересечения {x},{y}");
+
+
+
+
+void GetMatrixArray(double [,]matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble()*10;
+            matrix[i,j]=Math.Round(matrix[i,j],1);
+        }
+    }
+}
+
+void PrintMatrixArray(double[,] InArray)
+{
+    for (int i = 0; i < InArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < InArray.GetLongLength(1); j++)
+        {
+            Write($"{InArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
