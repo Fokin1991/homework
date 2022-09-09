ae979b0fa5a3183bbe573826753b3025146b9c4f
Ivan <soldren2@gmail.com>
2022-09-09 23:52:51 +0300

    add

--- Program.cs
+++ Program.cs
@@ -2,32 +2,54 @@
 
 
 
-
 using static System.Console;
 using System.Linq;
 Clear();
-WriteLine("Введите  размер  массива");
-WriteLine();
-WriteLine();
 int n=Convert.ToInt32(ReadLine());
 int m=Convert.ToInt32(ReadLine());
-int [,] array= new int [n,m];
-Gematrix(array);
+WriteLine();
+WriteLine();
 
-PrintArray(array);
-
-
+int [,] array=new int[n,m];
 
 
+Getmatrix(array);
+
+PrintMatrix(array);
+
+int summ=Int32.MaxValue;
+int  digit=0;
+
+
+WriteLine();
+WriteLine();
+WriteLine();
+WriteLine();
+WriteLine();
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{ 
+    int  result=0;
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+        result=(result+array[i,j]);
+}
+if (result<summ)
+{
+    summ=result;
+    digit++;
+}
+
+} 
+WriteLine($"{summ}");
 
 
 
 
 
-
-
-
-void Gematrix(int [,] MatrixArray)
+void Getmatrix(int [,]MatrixArray)
 {
     for (int i = 0; i < MatrixArray.GetLength(0); i++)
     {
@@ -35,21 +57,33 @@ void Gematrix(int [,] MatrixArray)
         {
             MatrixArray[i,j]=new Random().Next(1,11);
         }
+        
     }
 }
 
 
 
-void PrintArray(int [,] InArray)
+    
+
+
+
+
+void PrintMatrix(int [,] InArray)
+
 {
     for (int i = 0; i < InArray.GetLength(0); i++)
     {
-       for (int j = 0; j < InArray.GetLength(1); j++)
-       {
-         Write($"{InArray} ");
-       } 
-    } WriteLine();
+        for (int j = 0; j < InArray.GetLength(1); j++)
+        {
+          Write($"{InArray[i,j]} ");
+        }
+        WriteLine();
+    }
+
+
 }
+
+
 
 
 
