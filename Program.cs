d75b730c51496390f26328a569cd0ea30de47594
Ivan <soldren2@gmail.com>
2022-09-09 20:35:08 +0300

    add

--- Program.cs
+++ Program.cs
@@ -10,66 +10,65 @@ WriteLine("Введите  размер массива");
 
 
 
-Console.Clear();
-Console.WriteLine($"Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
-Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
-int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int range = InputNumbers("Введите диапазон: от 1 до ");
+int n=Convert.ToInt32(ReadLine());
+int m=Convert.ToInt32(ReadLine());
+int [,] array=new int[n,m];
+Gematrix(array);
+WriteLine();
+WriteLine();
+PrintArray(array);
+WriteLine();
+WriteLine();
 
-int[,] array = new int[m, n];
-CreateArray(array);
-WriteArray(array);
+WriteLine();
+WriteLine();
 
-Console.WriteLine($"\nОтсортированный массив: ");
-OrderArrayLines(array);
-WriteArray(array);
 
-void OrderArrayLines(int[,] array)
+for (int i = 0; i <array.GetLength(0); i++)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < array.GetLength(1)-1; j++)
     {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
-      {
-        if (array[i, k] < array[i, k + 1])
+       
+        for (int y = 0; y < array.GetLength(1)-1; y++)
         {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
+            if(array[i,y]<array[i,y+1])
+            {
+                int result=0;
+
+                result=array[i,y];
+                array[i,y]=array[i,y+1];
+                array[i,y+1]=result;
+            } 
         }
-      }
     }
-  }
 }
+PrintArray(array);
 
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
 
-void CreateArray(int[,] array)
+
+
+void Gematrix(int[,]MatrixArray)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < MatrixArray.GetLength(0); i++)
     {
-      array[i, j] = new Random().Next(range);
+        for (int j = 0; j < MatrixArray.GetLength(1); j++)
+        {
+            MatrixArray[i,j]=new Random().Next(1,11);
+        }
     }
-  }
 }
 
-void WriteArray(int[,] array)
+
+
+
+void PrintArray(int [,]InArray)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i <InArray.GetLength(0) ; i++)
     {
-      Console.Write(array[i, j] + " ");
+        for (int j = 0; j < InArray.GetLength(1); j++)
+        {
+            Write($"{InArray[i,j]} ");
+        }
+        WriteLine();
     }
-    Console.WriteLine();
-  }
 }
