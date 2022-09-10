80f414573323bb5f50f2bac3557a40d2647e924f
Ivan <soldren2@gmail.com>
2022-09-11 00:11:59 +0300

    add

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//Задача 58
+﻿//Задача 60
 
 
 
@@ -6,59 +6,69 @@
 using static System.Console;
 using System.Linq;
 Clear();
+int count1 = InputInt("Введите размерность 1: ");
+int count2 = InputInt("Введите размерность 2: ");
+int count3 = InputInt("Введите размерность 3: ");
+int countNums = 89;
+
+if (count1 * count2 * count3 > countNums)
+{
+    Console.Write("Массив слишком большой");
+    return;
+}
+
+int[,,] resultNums = Create3DMassive(count1, count2, count3);
+
+for (int i = 0; i < resultNums.GetLength(0); i++)
+{
+    for (int j = 0; j < resultNums.GetLength(1); j++)
+    {
+        for (int k = 0; k < resultNums.GetLength(2); k++)
+        {
+            Console.WriteLine($"[{i},{j},{k}] - {resultNums[i, j, k]}");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int[,,] Create3DMassive(int size1, int size2, int size3)
+{
+    int[,,] array = new int[size1, size2, size3];
+    int[] values = new int[countNums];
+    int num
+     = 10;
+    for (int i = 0; i < values.Length; i++)
+        values[i] = num
+        ++;
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        int randomInd = new Random().Next(0, values.Length);
+        int temp = values[i];
+        values[i] = values[randomInd];
+        values[randomInd] = temp;
+    }
+
+    int valueIndex = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = values[valueIndex++];
+            }
+        }
+    }
+    return array;
+}
+
+
 int InputInt(string output)
 {
     Console.Write(output);
     return int.Parse(Console.ReadLine()!);
 }
-
-void FillArrayRandomNumbers(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10); 
-        }
-    }
-}
-
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        Console.Write("[ ");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.Write("]");
-        Console.WriteLine("");
-    }
-}
-
-int size = InputInt("размерность матриц: ");
-int[,] matrixA = new int[size, size];
-int[,] matrixB = new int[size, size];
-FillArrayRandomNumbers(matrixA);
-FillArrayRandomNumbers(matrixB);
-int[,] matrixC = new int[size, size];
-
-for (int i = 0; i < size; i++)
-{
-    for (int j = 0; j < size; j++)
-    {
-        for (int k = 0; k < size; k++)
-        {
-            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
-        }
-    }
-}
-Console.WriteLine("Матрица - А");
-PrintArray(matrixA);
-Console.WriteLine();
-Console.WriteLine("Матрица - В");
-PrintArray(matrixB);
-Console.WriteLine();
-Console.WriteLine("Произведение матриц А*В");
-PrintArray(matrixC);
